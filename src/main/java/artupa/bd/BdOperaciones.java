69ffb8f13f5a57a6748f587c6692782b45eba681
Sendoa6 <sendoa.perez06@somo.eus>
2026-02-02 12:33:14 +0100

    eres mi martirio eres mi deliro yo soy tu soda pop

--- src/main/java/artupa/bd/BdOperaciones.java
+++ src/main/java/artupa/bd/BdOperaciones.java
@@ -212,4 +212,5 @@ public class BdOperaciones extends BdBase {
         }
         return correcto;
     }
+    
 }
---
+++ src/main/java/artupa/beans/Autor.java
@@ -0,0 +1,34 @@
+package artupa.beans;
+
+public class Autor {
+    
+    private int idAutor;
+    private String nombre;
+
+    // Constructor vacío
+    public Autor() {
+    }
+
+    // Constructor con campos
+    public Autor(int idAutor, String nombre) {
+        this.idAutor = idAutor;
+        this.nombre = nombre;
+    }
+
+    // Getters y Setters
+    public int getIdAutor() {
+        return idAutor;
+    }
+
+    public void setIdAutor(int idAutor) {
+        this.idAutor = idAutor;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
